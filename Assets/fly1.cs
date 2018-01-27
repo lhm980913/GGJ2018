261dcacf67b7bb1958784499e73f2e1e1130d61c
lhm980913 <31833286+lhm980913@users.noreply.github.com>
2018-01-27 11:51:37 +0800

    demo

---
+++ Assets/fly1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fly1 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (water.tem == 4|| water.tem == 3)
+        {
+            if (collision.tag == "Player")
+            {
+                collision.GetComponent<Rigidbody2D>().gravityScale = 0;
+                collision.transform.position += new Vector3(0, 4 * Time.deltaTime);
+                collision.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0.1f));
+            }
+        }
+        else collision.GetComponent<Rigidbody2D>().gravityScale = 8;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (water.tem == 4|| water.tem == 3)
+        {
+            if (collision.tag == "Player")
+            {
+                //collision.transform.position+= new Vector3(0, 2*Time.deltaTime);
+                collision.GetComponent<Rigidbody2D>().gravityScale = 8;
+            }
+        }
+    }
+}
---
+++ Assets/water1.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water1 : MonoBehaviour {
+    public PlatformEffector2D pl;
+
+    public float speed_steam;
+    public water water_1;
+    int a;
+    public GameObject[] steam;
+    float ttime = 0;
+    float rate = 0;
+    public float whide;
+    int i = 0;
+    int n = 1;
+    public float lenth;
+    public GameObject ice;
+    private bool iswater = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a = (int)water.tem;
+        ttime += Time.deltaTime;
+
+        //if(Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    tem--;
+
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    tem++;
+
+        //}
+        //if (tem == 5) tem = 4;
+        //if (tem == 0) tem = 1;
+
+        switch (a)
+        {
+            case 1:
+                {
+                    beeice();
+                }
+                break;
+            case 2:
+                {
+                    beewater();
+                }
+                break;
+            case 3:
+                {
+                    beesteam();
+                }
+                break;
+            case 4:
+                {
+                    beesteam();
+
+
+                }
+                break;
+        }
+    }
+    void beeice()
+    {
+        iswater = false;
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        gameObject.transform.GetChild(0).gameObject.active = false;
+        ice.active = true;
+    }
+    void beesteam()
+    {
+        ice.active = false;
+        iswater = false;
+        if (ttime > rate)
+        {
+
+            rate = ttime + speed_steam;
+
+            Instantiate(steam[i], new Vector3(transform.position.x - whide + Random.Range(0, whide), transform.position.y - 3), new Quaternion());
+            Instantiate(steam[n], new Vector3(transform.position.x - whide + Random.Range(0, whide), transform.position.y - 2), new Quaternion());
+            i++; n++;
+            if (i == 3) i = 0;
+            if (n == 3) n = 0;
+        }
+    }
+    void beewater()
+    {
+        gameObject.transform.GetChild(0).gameObject.active = true;
+        this.GetComponent<BoxCollider2D>().isTrigger = true;
+        ice.active = false;
+        iswater = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (iswater)
+        {
+            if (collision.tag == "Player")
+            {
+
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
