3d81dd31aaae238299cbedac7647dd6d54d642d1
lhm980913 <31833286+lhm980913@users.noreply.github.com>
2018-01-27 18:18:40 +0800

    egdstgdgxbhdff

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -11,6 +11,6 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        this.transform.Rotate(new Vector3(0, 0, 1f));
 	}
 }
---
+++ Assets/aaaaaaa.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aaaaaaa : MonoBehaviour {
+    public GameObject aaaaaaaaaaaaaaaaaaaaaaaaa;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(aaaaaaaaaaaaaaaaaaaaaaaaa.transform.position.x-5, aaaaaaaaaaaaaaaaaaaaaaaaa.transform.position.y-2);
+	}
+}
