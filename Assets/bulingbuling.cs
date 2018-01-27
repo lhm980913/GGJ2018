295cafbad42ab621ad8a84abb252ca371d4d874f
lhm980913 <31833286+lhm980913@users.noreply.github.com>
2018-01-27 16:41:00 +0800

    啊

---
+++ Assets/bulingbuling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulingbuling : MonoBehaviour {
+    float t = 0;
+    public float max, min;
+   public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Mathf.Lerp(min, max, t));
+        t = t + speed * Time.deltaTime;
+        if (t > 1) speed = -0.5f;
+        if (t < 0) speed = 0.5f;
+    }
+}
---
+++ Assets/dongcidaci.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dongcidaci : MonoBehaviour {
+    public float x, y;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (water.tem == 4)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + x, this.transform.position.y + y);
+            if (x > 0 && y > 0) x = -x;
+            else
+            if (x < 0 && y > 0) y = -y;
+            else
+            if (x < 0 && y < 0) x = -x;
+            else
+            if (x > 0 && y < 0) y = -y;
+        }
+        if (water.tem == 3)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + x/2, this.transform.position.y + y/2);
+            if (x > 0 && y > 0) x = -x;
+            else
+            if (x < 0 && y > 0) y = -y;
+            else
+            if (x < 0 && y < 0) x = -x;
+            else
+            if (x > 0 && y < 0) y = -y;
+        }
+
+    }
+}
